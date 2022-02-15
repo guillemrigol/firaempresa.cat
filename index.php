84a7ab5017736a68935e08d307c439b976bc6631
Guillem Rigol <grigol@xtec.cat>
2022-02-15 17:55:52 +0100

    GR 15/02 17:55

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ca" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+    <?php include('collab/1.php') ?>
+    <?php include('collab/2.php') ?>
+    <?php include('collab/3.php') ?>
+    <?php include('collab/4.php') ?>
+    <?php include('collab/5.php') ?>
+    <?php include('collab/6.php') ?>
+    <?php include('collab/7.php') ?>
+    <?php include('collab/8.php') ?>
+    <?php include('collab/9.php') ?>
+    <?php include('collab/10.php') ?>
+    <?php include('collab/11.php') ?>
+    <?php include('collab/12.php') ?>
+    <?php include('collab/13.php') ?>
+    <?php include('collab/14.php') ?>
+    <?php include('collab/15.php') ?>
+    <?php include('collab/16.php') ?>
+    <?php include('collab/17.php') ?>
+    <?php include('collab/18.php') ?>
+    <?php include('collab/19.php') ?>
+    <?php include('collab/20.php') ?>
+    <?php include('collab/21.php') ?>
+    <?php include('collab/22.php') ?>
+    </ul>
+  </body>
+</html>
