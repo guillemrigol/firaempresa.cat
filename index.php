d80845219c19097f12c57f7ab8e2195483c933bb
Guillem Rigol <grigol@xtec.cat>
2022-02-18 19:47:35 +0100

    GR 18/02 19:47

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<?php include(header.php) ?>
-<?php include(estils.php) ?>
-<?php include(footer.php) ?>
+<?php include("header.php") ?>
+<?php include("estils.php") ?>
+<?php include("footer.php") ?>
