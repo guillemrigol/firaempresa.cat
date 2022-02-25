20a6dbebfa16ba32dd9544b0bec392d08f86cfa5
alexrodriguezc <alexgamergi@gmail.com>
2022-02-25 19:25:27 +0100

    AR 25/02 19:25

--- header.php
+++ header.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8">
     <title></title>
   </head>
-<header>
-  <nav>
-    <div><h3>Registre</h3></div>
-    <div><h3>Inici de sesió</h3></div>
-    <div>Menu</div>
-  </nav>
-</header>
-  <body>
+<body>
+  <header>
+    <nav>
+      <div><h3>Registre</h3></div>
+      <div><h3>Inici de sesió</h3></div>
+      <div>Menu</div>
+    </nav>
+  </header>
