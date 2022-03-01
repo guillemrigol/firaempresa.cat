8b0d14ed47390c7631e1cfd4ad598acca35a9c0b
kevincavi <kevincasanovasvila@gmail.com>
2022-03-01 17:45:44 +0100

    KC 01/03 15:45

--- estils.php
+++ estils.php
@@ -1,13 +1,14 @@
 <section>
-  <h1>Headline5</h1>
-  <h2>Headline4</h2>
+  <h1>Headline1</h1>
+  <h2>Headline2</h2>
   <h3>Headline3</h3>
-  <h4>Headline2</h4>
-  <h5>Headline1</h5>
+  <h4>Headline4</h4>
+  <h5>Headline5</h5>
+  <h6>Headline6</h6>
+
 
   <p>
-    It is a long established fact that a reader will be distracted
-    by the readable content of a page when looking at its layout.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tellus diam, posuere eu nisl non, finibus fringilla velit. Nulla facilisi. Sed a quam eu risus fringilla ornare eget at eros. Aliquam eget aliquam nulla. Donec eu finibus nisi. Morbi scelerisque erat quis mi congue, quis rutrum sapien volutpat. Pellentesque egestas convallis purus, eget posuere orci mattis sodales. Aliquam porta malesuada tempor. Suspendisse a elit nisl. Aliquam laoreet lacus nec tortor tincidunt, vel congue lacus posuere. Sed et massa eu risus rhoncus auctor vitae a urna. Maecenas eget lacus purus.
   </p>
 
   <ol>
