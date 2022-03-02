64d4142753568aac507aaeda1a076bfdfa7dae18
ayoubeel <ayoubeelathmany@gmaul.com>
2022-03-02 19:51:23 +0100

    AE 02/03 19:51

--- estils.php
+++ estils.php
@@ -11,6 +11,10 @@
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tellus diam, posuere eu nisl non, finibus fringilla velit. Nulla facilisi. Sed a quam eu risus fringilla ornare eget at eros. Aliquam eget aliquam nulla. Donec eu finibus nisi. Morbi scelerisque erat quis mi congue, quis rutrum sapien volutpat. Pellentesque egestas convallis purus, eget posuere orci mattis sodales. Aliquam porta malesuada tempor. Suspendisse a elit nisl. Aliquam laoreet lacus nec tortor tincidunt, vel congue lacus posuere. Sed et massa eu risus rhoncus auctor vitae a urna. Maecenas eget lacus purus.
   </p>
 
+  <p>
+    Cras porttitor enim sit amet nisl sagittis, eget molestie turpis dignissim. Praesent nec massa porttitor, posuere mi eu, commodo orci. Vivamus imperdiet mauris ut venenatis tincidunt. Suspendisse id pretium lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean vitae molestie est. Aliquam lectus felis, faucibus nec porta ac, ullamcorper ac dolor. Quisque eu urna volutpat, vehicula sem hendrerit, semper felis. Proin orci risus, elementum ut placerat nec, porta at enim. Aliquam at dictum elit.
+  </p>
+
   <ol type="circle">
     <li>Lorem Ipsum </li>
     <li>Lorem Ipsum </li>
@@ -40,7 +44,7 @@
       </ul>
     </li>
   </ul>
-  
+
   <div>
     <button type="buttom">lore ipsum</button>
   </div>
