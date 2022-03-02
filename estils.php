6908dc747f575439886d994c8ad217be905f6034
Polrb7 <romeuptona@gmail.com>
2022-03-02 19:34:14 +0100

    PR 02/03 19:34

--- estils.php
+++ estils.php
@@ -11,7 +11,7 @@
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tellus diam, posuere eu nisl non, finibus fringilla velit. Nulla facilisi. Sed a quam eu risus fringilla ornare eget at eros. Aliquam eget aliquam nulla. Donec eu finibus nisi. Morbi scelerisque erat quis mi congue, quis rutrum sapien volutpat. Pellentesque egestas convallis purus, eget posuere orci mattis sodales. Aliquam porta malesuada tempor. Suspendisse a elit nisl. Aliquam laoreet lacus nec tortor tincidunt, vel congue lacus posuere. Sed et massa eu risus rhoncus auctor vitae a urna. Maecenas eget lacus purus.
   </p>
 
-  <ol type="circle">
+  <ol type="I">
     <li>Lorem Ipsum </li>
     <li>Lorem Ipsum </li>
     <li>Lorem Ipsum </li>
@@ -40,7 +40,7 @@
       </ul>
     </li>
   </ul>
-  
+
   <div>
     <button type="buttom">lore ipsum</button>
   </div>
