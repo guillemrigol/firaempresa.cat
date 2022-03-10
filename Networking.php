63c8b7400ff87db526af7ca2b515c13f651478b0
Proskin365 <theproskin365@gmail.com>
2022-03-10 19:59:58 +0100

    RA 10/03/22 19:59

--- Networking.php
+++ Networking.php
@@ -8,5 +8,31 @@
 
   Les empreses durant el  networking tindran a la seva disposició una taula per fer el networking.
   </p>
+  
+  <p>Funcionament del networking</p>
 
+  <p><b>Abrics i motxilles en general l'únic lloc on podreu deixar coses és a sota la pròpia taula de networking.</b> Per tant dueu lo just i imprescindible.
+
+
+  <b>Podeu portar material exposat fora de la propia taula, sempre i quan l’espai que ocupi  no interfereixi el bon funcionament del networking.</b>
+
+
+
+  Podeu adequar el vostre espai assignat amb cartells i l’atrezzo que creieu convenient, per tal de donar una imatge professional i atractiva per captar l’interès dels vostres clients.
+
+
+
+  Hi haurà un premi al millor espai i actuació del vostre networking a les 3 millors empreses.
+
+
+
+  Penseu que no hi ha endolls disponibles, així que porteu material que us permeti treballar sense ordinador, o ordinadors que tinguin càrrega suficient pel temps que durarà el networking.
+
+
+
+  <b>Documents que cal portar:</b> la idea del networking és comprar, vendre i fer contractes de compra-venda.<b> Porteu els documents en paper que necessiteu per emplenar al moment.</b> No us oblideu del catàleg de productes o el format que creieu adequat per mostrar els vostres productes. També es podrà portar ordinador amb tot el material digital necessari.
+
+
+
+  Recordeu,<b> sigueu oberts a l'hora de comprar i vendre.</b> Ningú us donarà un premi per ser els més rics de la plataforma, però guanyareu molt si sortiu amb una bona experiència i amb les aptituds reforçades.</p>
 <?php include("footer.php") ?>
