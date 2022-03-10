4022bda746b87d9574d0353918741fab732da1c8
Nil <kickcreator@gmail.com>
2022-03-10 19:47:52 +0100

    NV 10/03 19:47

--- inici.php
+++ inici.php
@@ -7,16 +7,31 @@
 
   <style media="screen">
 
-nav.1{
+nav.carcasa{
   display: flex;
   position: relative;
 }
 
+div.logofira{
+  top: 50%;
+  left: 50%;
+  display: block;
+  position: absolute;
+}
+
+div.logolletres{
+  top: 50%;
+  left: 50%;
+  background-color: black;
+}
+
   </style>
   <body>
-    <nav id="1">
+    <nav id="carcasa">
     <div>
       <img class="logolletres" src="assets/svg/logo02.svg" />
+    </div>
+    <div>
       <img class="logofira" src="assets/svg/iconset/Logo.svg" />
   </div>
 </nav>
