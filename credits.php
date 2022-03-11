5da42290ab300f63e01be546f8f1514af8e8ce8a
Amiinnee <aelathmany@gmailcom>
2022-03-11 19:36:51 +0100

    AA 11/03 19:37

--- credits.php
+++ credits.php
@@ -3,14 +3,14 @@
 
 <section>
   <article>
-<div>
-  <p>Aquesta página web esta feta pels alumnes de Grau Mitjà de informàtica de segon any de la Salle Manlleu</p>
 
-  <p>Aquesta página web esta feta pels alumnes de Grau Mitjà de informàtica de la Salle Manlleu</p>
+  <p>Aquesta página web esta feta pels alumnes de Grau Mitjà de informàtica de segon any de La Salle Manlleu</p>
 
-  <p>Aquesta página web esta feta pels alumnes de Grau Mitjà de informàtica de la Salle Manlleu</p>
+  <p>Aquesta página web esta feta pels alumnes de Grau Mitjà de informàtica de La Salle Manlleu</p>
 
-</div>
+  <p>Aquesta página web esta feta pels alumnes de Grau Mitjà de informàtica de La Salle Manlleu</p>
+
+
 </section>
 </article>
 
