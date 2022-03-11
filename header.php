059e003382cce79483a61d49b5b479a92fae45cd
Guillem Rigol <grigol@xtec.cat>
2022-03-11 20:06:50 +0100

    GR 11/03 20:06

--- header.php
+++ header.php
@@ -1,25 +1,26 @@
 <!DOCTYPE html>
 <html lang="ca" dir="ltr">
   <head>
-    <meta charset="utf-8">
-    <title></title>
+    <meta charset="utf-8" />
+    <title>F I R A E M P R E S A . C A T</title>
     <link rel="stylesheet" type="text/css"  href="style.css" />
+    <script src="functions.js"></script>
   </head>
 <body>
+  <nav id="main">
+    <button onclick="window.open('inici.php', '_self')">Inici</button>
+    <button onclick="window.open('horaris.php', '_self')">Horaris</button>
+    <button onclick="window.open('presentacions.php', '_self')">Presentacions</button>
+    <button onclick="window.open('networking.php', '_self')">Networking</button>
+    <button onclick="window.open('credits.php', '_self')">Crèdits</button>
+  </nav>
   <a name="capçalera"></a>
   <header>
-    <nav>
       <div class="right">
       <div id="registre">Registre</div>
       <div id="inici">Inici de sessió</div>
       <img class="persona" src="assets/svg/iconset/IconPersona.svg" />
       </div>
-      <img class="logo" src="assets/svg/logo02.svg" />
+      <img class="logo" src="assets/svg/logo02.svg" onclick="window.open('inici.php', '_self')"/>
       <input type="button" value="" class="menu" />
-      <div id="barra-menu">
-        <h1 class="lletra">Inici</h1>
-        <h1 class="lletra">Informació</h1>
-        <h1 class="lletra">Contacte</h1>
-      </div>
-    </nav>
   </header>
