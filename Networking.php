d87a86574dc91bb69915c1ca82b0fcc2dea9583b
Kevincaballero04 <kevinjako5@gmail.com>
2022-03-11 20:17:06 +0100

    KCN 11/03 20:16

--- Networking.php
+++ Networking.php
@@ -1,67 +1,53 @@
 
 <?php include("header.php") ?>
-<<<<<<< HEAD
-<0>Funcionament del networking</p>
 
-<p><b>Abrics i motxilles en general: l'únic lloc on podreu deixar coses és a sota la pròpia taula de networking.</b> Per tant dueu lo just i imprescindible.
+<h1>5.1 Espai disponible i atrezzo</h1>
 
+<p>L’espai on es realitzarà el networking serà l’auditori-esglèsia.<br></b>
+Les empreses durant el  networking tindran a la seva disposició una taula per fer el networking.
+</p>
 
-<b>Podeu portar material exposat fora de la propia taula, sempre i quan l’espai que ocupi  no interfereixi el bon funcionament del networking.</b>
-
-
-
-Podeu adequar el vostre espai assignat amb cartells i l’atrezzo que creieu convenient, per tal de donar una imatge professional i atractiva per captar l’interès dels vostres clients.
-
-
-
-Hi haurà un premi al millor espai i actuació del vostre networking a les 3 millors empreses.
-
-
-
-Penseu que no hi ha endolls disponibles, així que porteu material que us permeti treballar sense ordinador, o ordinadors que tinguin càrrega suficient pel temps que durarà el networking.
+<h2>5.2 Funcionament del networking</h2>
+<p><b>Abrics i motxilles en general: l'únic lloc on podreu deixar coses és a sota la pròpia taula de networking.</b> Per tant dueu lo just i imprescindible.</p>
+<p><b>Podeu portar material exposat fora de la propia taula, sempre i quan l’espai que ocupi  no interfereixi el bon funcionament del networking.</b></p>
+<p>Podeu adequar el vostre espai assignat amb cartells i l’atrezzo que creieu convenient, per tal de donar una imatge professional i atractiva per captar l’interès dels vostres clients.</p>
+<p>Hi haurà un premi al millor espai i actuació del vostre networking a les 3 millors empreses.</p>
+<p>Penseu que no hi ha endolls disponibles, així que porteu material que us permeti treballar sense ordinador, o ordinadors que tinguin càrrega suficient pel temps que durarà el networking.</p>
+<p><b>Documents que cal portar:</b> la idea del networking és comprar, vendre i fer contractes de compra-venda.<b> Porteu els documents en paper que necessiteu per emplenar al moment.</b> No us oblideu del catàleg de productes o el format que creieu adequat per mostrar els vostres productes. També es podrà portar ordinador amb tot el material digital necessari.</p>
+<p>Recordeu,<b> sigueu oberts a l'hora de comprar i vendre.</b> Ningú us donarà un premi per ser els més rics de la plataforma, però guanyareu molt si sortiu amb una bona experiència i amb les aptituds reforçades.</p>
 
 
+<h3>5.3 Consells per un bon networking</h3>
 
-<b>Documents que cal portar:</b> la idea del networking és comprar, vendre i fer contractes de compra-venda.<b> Porteu els documents en paper que necessiteu per emplenar al moment.</b> No us oblideu del catàleg de productes o el format que creieu adequat per mostrar els vostres productes. També es podrà portar ordinador amb tot el material digital necessari.
+<p>•  Mostreu-vos oberts, accessibles i amb confiança. Cal anar a  la trobada amb la vostra millor actitud.</p>
+<p>•	Feu un estant original, atractiu i que capti l’atenció dels visitants de la Firaempresa.</p>
+<p>•	Penseu com podeu impactar sobre els vostres clients, visitants...  per tal que s’interessin per la vostra empresa: demostracions, ofertes, regals, sorteig, descomptes...</p>
+<p>•	Aprofita qualsevol oportunitat per tenir un detall amb els teus contactes i fidelitzar-los.</p>
+<p>•	És important que doneu una imatge corporativa com a empresa.</p>
+<p>•	Utilitzeu una presentació ràpida de qui sou i què oferiu, és a dir, un bon elevator pitch.</p>
+<p>•	Busqueu contactes que siguin molt actius fent networking, segur que serà fàcil de connectar amb ells.</p>
+<p>•	Tingueu un tracte amable i assertiu vers als vostres possibles clients, la primera impressió és fonamental.</p>
+<p>•	Recopila la màxima informació possible de les empreses: nom empresa , dades de contacte, què ofereix l’empresa, preus, condicions, etc.</p>
+<p>•	Marqueu-vos un objectiu de vendes i compres que sigui realista i viable. No es tracta de recollir molts contactes i no acabar realitzant cap operació.</p>
+<p>•	El networking no té límits en l’espai ni el temps. Però necessita de la inversió de temps i esforços continus per tal que el resultat dels contactes obtinguts prosperin, creixin i es mantinguin en el temps.</p>
+<p>•	Gaudiu de l’experiència i de tenir l’oportunitat de connectar amb les empreses de manera presencial
+</p>
+
+<h4>5.4 Criteris de valoració del Networking :</h4>
+<p>1) Originalitat de l’ estant.</p>
+<p>2) Saber vendre el producte i/o servei ( Client/proveïdor).</p>
+<p>3) Imatge corporativa de l’empresa.</p>
+<p>4) Recollida d’ informació i contactes .</p>
+
+<5>RÚBRICA DEL NETWORKING</h5>
+<img class="taula-presentacions" src="assets/img/taulaaaaa.png"/>
 
 
-
-Recordeu,<b> sigueu oberts a l'hora de comprar i vendre.</b> Ningú us donarà un premi per ser els més rics de la plataforma, però guanyareu molt si sortiu amb una bona experiència i amb les aptituds reforçades.</p>
-=======
-  <p>
-  5.1 - Espai disponible i atrezzo
- </p>
- <p>
-  L’espai on es realitzarà el networking serà l’auditori-esglèsia.
->>>>>>> bd309403f8e31949b7f4e61601a08f9e2c7e322b
-
-  Les empreses durant el  networking tindran a la seva disposició una taula per fer el networking.
-  </p>
-
-  <h1>Funcionament del networking</h1>
-
-  <p><b>Abrics i motxilles en general l'únic lloc on podreu deixar coses és a sota la pròpia taula de networking.</b> Per tant dueu lo just i imprescindible.
-
-
-  <b>Podeu portar material exposat fora de la propia taula, sempre i quan l’espai que ocupi  no interfereixi el bon funcionament del networking.</b>
-
-
-
-  Podeu adequar el vostre espai assignat amb cartells i l’atrezzo que creieu convenient, per tal de donar una imatge professional i atractiva per captar l’interès dels vostres clients.
+<h6>5.5 Premis del Networking :</h6>
+<p>Per poder registrar-vos a la Trobada, només cal que accediu a la web firaempresa</p>
 
 
 
-  Hi haurà un premi al millor espai i actuació del vostre networking a les 3 millors empreses.
 
 
-
-  Penseu que no hi ha endolls disponibles, així que porteu material que us permeti treballar sense ordinador, o ordinadors que tinguin càrrega suficient pel temps que durarà el networking.
-
-
-
-  <b>Documents que cal portar:</b> la idea del networking és comprar, vendre i fer contractes de compra-venda.<b> Porteu els documents en paper que necessiteu per emplenar al moment.</b> No us oblideu del catàleg de productes o el format que creieu adequat per mostrar els vostres productes. També es podrà portar ordinador amb tot el material digital necessari.
-
-
-
-  Recordeu,<b> sigueu oberts a l'hora de comprar i vendre.</b> Ningú us donarà un premi per ser els més rics de la plataforma, però guanyareu molt si sortiu amb una bona experiència i amb les aptituds reforçades.</p>
 <?php include("footer.php") ?>
