7e186cf3231e170f5e26549b1be42b07b610b8fb
Guillem Rigol <grigol@xtec.cat>
2022-03-17 16:52:08 +0100

    GR 17/03 16:52

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 <section>
   <article>
     <h1>Benvinguda<h1>
-      <p></p>
+    <p>La Fira empresa aquest any es farà al nostre centre de la Salle Manlleu. La pandemia va fer que el curs passat es fes online, i ara ens toca recuperar-la de manera presencial.</p>
+    <p>Les empreses simulades tindran l’oportunitat de trobar-se i presentar les seves propostes comercials, 17 empreses de CFGM, CFGS i Batxillerat hi podran participar.</p>
+    <p>La trobada té diferents objectius, però els principals són: millorar les habilitats comunicatives, intentar superar el pànic escènic i aconseguir que l’alumnat adopti una actitud professional.</p>
+    <p>Per aconseguir-ho es plantegen dues activitats bàsiques de les que parlarem amb més detall: presentacions i networking.</p>
+    <p>Per altra banda, volem que tot l’alumnat que participa a La Fira tingui possibilitat de millorar en els objectius indicats. És per aquest motiu que no es fa cap filtratge previ, ni fase de selecció de les presentacions que es faran durant l’esdeveniment.</p>
+    <p>Les empreses que vénen a la Fira es comprometen a ser-hi presents durant tot l’esdeveniment, des de primera hora fins al final del lliurament de premis.</p>
+    <p>La Fira es farà a l’auditori-esglèsia de l’escola, tant les presentacions com el networking, i es farà en dos dies diferents, el dimarts 22 de març ,les empreses podran presentar les seves propostes comercials i el dimarts 29 de març farem el networking.</p>
   </article>
 </section>
 <?php include("footer.php") ?>
